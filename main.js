469a348d4561358c1700bf5a7867b853e3d8eafc
Miunn <remcaulier@orange.fr>
2024-02-03 10:27:05 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron';
+import { join } from 'path';
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:3000');
+
+  mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});

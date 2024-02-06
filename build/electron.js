a087c034f064db21b0a2776827aba476192fe745
Miunn <remcaulier@orange.fr>
2024-02-06 15:06:25 +0100

    React build

---
+++ build/electron.js
@@ -0,0 +1,76 @@
+import { app, BrowserWindow, protocol } from 'electron';
+import isDev from 'electron-is-dev';
+import path from 'path';
+import url from "url";
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // In production, set the initial browser path to the local bundle generated
+  // by the Create React App build process.
+  // In development, set it to localhost to allow live/hot-reloading.
+  const appURL = app.isPackaged
+    ? url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    : "http://localhost:3000";
+  mainWindow.loadURL(appURL);
+ 
+  // Automatically open Chrome's DevTools in development mode.
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools();
+  }
+  
+  mainWindow.on('closed', () => (mainWindow = null));
+}
+
+// Setup a local proxy to adjust the paths of requested files when loading
+// them from the local production bundle (e.g.: local fonts, etc...).
+function setupLocalFilesNormalizerProxy() {
+  protocol.registerHttpProtocol(
+    "file",
+    (request, callback) => {
+      const url = request.url.substr(8);
+      callback({ path: path.normalize(`${__dirname}/${url}`) });
+    },
+    (error) => {
+      if (error) console.error("Failed to register protocol");
+    },
+  );
+}
+
+
+app.on('ready', () => {
+  createWindow();
+  setupLocalFilesNormalizerProxy();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
